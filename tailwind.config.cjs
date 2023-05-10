269a9bf6b4c2a5a9e234272bfbed32b60b5b52dc
vaibhavrajsingh2001 <vaibhavrajsingh2001@gmail.com>
2023-05-10 16:49:10 +0530

    feat: add tailwind typography
    Signed-off-by: vaibhavrajsingh2001 <vaibhavrajsingh2001@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require('daisyui')],
-};
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['dracula']
+  }
+}
